16920ba660a22eb1a6606e1cd02a0712c2bda9e3
Changlin Li <mail@changlinli.com>
2024-03-30 15:50:36 -0700

    First commit!

---
+++ README.md
@@ -0,0 +1,7 @@
+# Example Zokka Package
+
+This is an example project to illustrate the simplest of Zokka packages! This
+should be familiar to anyone who has worked with an Elm library before.
+
+This package mainly provides a convenient interface to test out Zokka's custom
+repositories. Try uploading this as a new library!
---
+++ src/Example.elm
@@ -0,0 +1,14 @@
+module Example exposing (someConstant)
+
+{-| An example library. This is mainly to illustrate Zokka's publishing process
+and has no useful function otherwise.
+
+# A fun little constant
+@docs someConstant
+
+-}
+
+{-| A single constant, just to illustrate this library!
+-}
+someConstant : Int
+someConstant = 2
